8d41075cdc0ff3f1df78a2d599e1a558b12cf30f
Theo Rodriguez <rodriguez.theo@outlook.com>
2023-11-06 23:47:54 +0100

    adding utils verifyPassword

---
+++ authentication/src/utils/verifyPassword.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+
+export const verifyPassword = async (
+  password: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  try {
+    return await bcrypt.compare(password, hashedPassword);
+  } catch (e) {
+    return false;
+  }
+};
